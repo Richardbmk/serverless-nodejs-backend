c52592db586a9e12faf6f9c996c3a43d812ac6b1
Ricardo <rdobmk@gmail.com>
2021-01-30 14:46:13 +0100

    all lambda function implemented

---
+++ src/update-recipe.js
@@ -0,0 +1,48 @@
+/**
+ * Route: PATCH /recipe
+*/
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2' });
+
+const util = require('./util');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.RECIPE_TABLE;
+
+exports.handler = async (event) => {
+    try {
+        let item = JSON.parse(event.body).Item;
+        item.user_id = util.getUserId(event.headers);
+        item.user_name = util.getUserName(event.headers);
+
+        let data = await dynamodb.put({
+            TableName: tableName,
+            Item: item,
+            ConditionExpression: '#t = :t',
+            ExpressionAttributeNames: {
+                '#t': 'timestamp'
+            },
+            ExpressionAttributeValues: {
+                ':t': item.timestamp
+            }
+        }).promise();
+
+        return {
+            statusCode: 200,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify(item)
+        }
+    } catch (err) {
+        console.log("Error", err);
+        return {
+            statusCode: err.statusCode ? err.statusCode : 500,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify({
+                error: err.name ? err.name : "Exception",
+                message: err.message ? err.message : "Unknown error"
+            })
+        };
+    }
+}
+
